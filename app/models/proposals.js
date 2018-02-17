17e5176978cc60dcbf4dc69c2dd776248d4965dc
Rohan Mishra <rohanmishra2017@gmail.com>
2018-02-17 14:41:28 -0700

    Created Proposal schema and added to models

---
+++ app/models/proposals.js
@@ -0,0 +1,16 @@
+/*
+ * app/models/proposals.js
+ * Proposal schema
+ *
+ */
+
+export Proposal = {
+    Title : "",
+    Body  : "",
+    Anon : false,
+    Authors : [],
+    Comments : [],
+};
+
+module.exports = Proposal;
+
