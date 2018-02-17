17410f322f1785233bc978f9a6074db36d19ae45
SlickJohnson <liwa.johnson@gmail.com>
2018-02-17 13:45:53 -0700

    Impement qr decoding for qr codes sent via sms

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,8 @@ from flask import Blueprint, request, current_app as app
 from twilio.twiml.messaging_response import Message, MessagingResponse
 from twilio.rest import Client
 import requests
+import cv2
+import pyzbar.pyzbar as zbar
 
 main = Blueprint('main', __name__)
 
@@ -19,21 +21,43 @@ def index():
     message_sid = request.form['MessageSid']
     media_url = request.form['MediaUrl0']
     media_type = request.form['MediaContentType0']
-    print(media_url, media_type)
 
     # Interpret message.
     response = MessagingResponse()
     if message == 'hello':
         response.message = 'ethdenv woooo!'
     if media_url:
-        with open('output.jpg', 'wb') as image:
-            image.write(requests.get(media_url).content)
-        response.message = 'thank you for joining!'
-        
+        # Save the image of the qrcode the root dir.
+        _save_qrcode(media_url)
 
+        # Read qrcode.
+        # Only get 1st code. (ignore multiple qr codes in same image)
+        code = _decode_qrcode()
+        if not code:
+            response.message = 'Hmmm, my vision must be bad. Can you send me your qr code again?'
+        else:
+            response.message = 'All set up!'
+            print(code)
 
     # Send response to user as sms.
     client.messages.create(to=from_number, from_=account_num, body=response.message)
 
     return str(response)
 
+
+def _save_qrcode(media_url):
+    # Download image from twilio media url.
+    with open('qr-code.jpg', 'wb') as image:
+            image.write(requests.get(media_url).content)
+
+    # Open image as grayscale.
+    gray_image = cv2.imread('qr-code.jpg', cv2.IMREAD_GRAYSCALE)
+    # Convert to black and white.
+    _, bw_image = cv2.threshold(gray_image, 128, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+    # Swap black and white to make qrcode appear 
+    cv2.imwrite('qr-code.jpg', (255 - bw_image))
+
+
+def _decode_qrcode():
+    img = cv2.imread('qr-code.jpg')
+    return zbar.decode(img)
