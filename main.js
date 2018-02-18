e1225f578c58b19f57d6566cfc1ea9d3f5e25442
Rohan Mishra <rohanmishra2017@gmail.com>
2018-02-17 21:32:55 -0700

    Added express and start server.js

--- main.js
+++ main.js
@@ -6,8 +6,20 @@
 
 "use strict";
 
+const server = require("./config/initalizers/server");
+
 // load environment variables from .env
 require("dotenv").loads();
 
-// 
-
+// Initalize Modules
+async.series([
+    function startServer(callback) {
+        server(callback);
+    }], function(err) {
+        if (err) {
+            console.log(err); 
+        } else {
+            console.log("[Architect] Initalized Successfully"); 
+        }
+    }
+);
