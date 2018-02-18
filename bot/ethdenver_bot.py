8b51b2ace408af77480d15c0a5b8dae647feafc8
Tia King <tiajaneeking@gmail.com>
2018-02-17 20:38:17 -0800

    finished v1 of ethdenver chatbot

---
+++ bot/ethdenver_bot.py
@@ -0,0 +1,139 @@
+# hugotbot.py
+from nltk.chat.util import Chat, reflections
+import nltk
+
+
+
+pairs = [
+  [
+    r'(Hi|Hello|Hey)',
+    ['Hey Friend! What is your name?'],
+  ],
+  [
+    r'Im hungry',
+    ['Would you like me to call uber eats?'],
+
+  ],
+
+  [
+    r'(.*|I am| My name is)',
+    ["""
+      Nice to meet you, %1. I wish I had a name like that. I actually don't have a name yet...#awkward. 
+      Would you like to set up an Archethtect account? Type yes or no.
+      """],
+  ],
+	[
+		r'yes:account',
+		["""
+      gnarly! Please take a picture of your QR code. Send it to me and I'll do the rest! 
+      """],
+	],
+  [
+    r'no:account',
+    ["Why text me when you know you don't want me? Quit wasting my time, bye."],
+  ],
+  [
+    r'yes:qrcode',
+    ["""
+      Account created. Would you like to see a list of current proposals or learn more about Archethtect? 
+      Type proposals or arch.
+      """],
+  ],
+  [
+    r'no:qrcode',
+    ["I'm sorry. For some reason that QR code could not be read. Please try focusing the camera and take it again."],
+  ],
+  [
+    r'yes:proposals',
+    ["""
+      Here are a list of proposals on queue for voting. [Stronger Wifi @ EthDenver 2019, Shorter Opening Ceremonies, 
+      More Food Truck Options and more.] Which would you like to see? Type wife or ceremonies or food or more. 
+      If you'd like to search by proposal category, type category.
+      """],
+  ],
+  [
+    r'yes:learn',
+    ["""
+      Wow! 😳 Thanks for being interested in me. Here's a link to learn more. 
+      https://github.com/Meeshbhoombah/architect
+      """],
+  ],
+  [
+    r'yes:wifi',
+    [ 
+      """
+      It's no secret that despite the valient efforts of the EthDenver organizers that the wifi 
+      this year was just short of disastrous. Therefore, in preparation for next year's event, 
+      I propose that in addition to forging partnerships with A1 blockchain and cryptocurrency
+      companies, that EthDenver collaborate with Comcast/AT&T to ensure that the bandwith of
+      the wifi modems can accomodate the # of hackers in the space. This is especially imperative
+      because EthDenver will only grow in interest and demand in the upcoming years.
+
+      """],
+  ],
+  [
+    r'yes:ceremonies',
+    [
+      """ 
+      Opening Ceremonies are a crucial part of every hackathon as they set the mood for the rest
+      of the event. It's also an important time to talk about the community and the type of community
+      we as hackers want to build in this space. However, a 2-hour ceremony is abhorrently long. Hackathons
+      are for hacking and time is precious. I propose that in the future, opening ceremonies are no longer than 1 hour long. 
+
+    """],
+  ],
+  [
+    r'yes:food',
+    ["""
+    I'll be the first to say that the food truck line-up at EthDenver this year was phenomenal and exceeded my expectations. 
+    There were a variety of cuisines to choose from and the wait times were short. However, a lot of the food was really heavy.
+    As hackers we are sitting down for long periods of time with not much opportunity to walk around, it causes the body and mind
+    to be sluggish when hackathons require its' participants to keep a clear, sharp mind. I propose that for EthDenver 2019, the
+    organizers offer more healthy/vegan/vegetarian options.
+    """],
+  ],
+  [
+    r'yes:more',
+    ["link."],
+  ],
+  [
+    r'yes:category',
+    ["Here is a list of the available categories. [Sponsors, Venue, Location, Prizes.] From which category would you like to see proposals on queue for voting?"],
+  ],
+  [
+    r'yes:different category',
+    ["Sure thing. Here are the other available categories."],
+  ],
+  [
+    r'yes:specific proposal',
+    ["link."],
+  ]
+]
+
+reflections = {
+  'my name is' :'your name is',
+  'i was'      : 'you were',
+  'i'          : 'you',
+  "i'm"        : "you are",
+  "i'd"        : "you would",
+  "i've"       : "you have",
+  "i'll"       : "you will",
+  "my"         : "your",
+  "you are"    : "I am",
+  "you were"   : "I was",
+  "you've"     : "I have",
+  "you'll"     : "I will",
+  "your"       : "my",
+  "yours"      : "mine",
+  "you"        : "me",
+  "me"         : "you"
+}
+
+def ethdenver_bot():
+    print("Welcome to the EthDenver Bot! What is your name?")
+    chat = Chat(pairs, reflections)
+    chat.converse()
+
+
+if __name__ == "__main__":
+    ethdenver_bot()
